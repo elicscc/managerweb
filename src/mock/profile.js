8ad612d5cbb08d00b7e2e9f6abc58ddd8a7bcbd4
elicscc <2296711078@qq.com>
2020-03-02 00:55:50 +0800

    6

---
+++ src/mock/profile.js
@@ -0,0 +1,13 @@
+export default {
+    profile:{
+        "code" : "10000",
+        "success" : true,
+        "message" : "登录成功",
+        "data" : {
+            mobile:"15905533321",
+            username:"张三",
+            company:"XXXX有限公司12345678",
+            email:"elicscc@vip.qq.com",
+        }
+    }
+}
